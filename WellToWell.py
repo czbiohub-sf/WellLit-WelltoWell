8f10b05a955afd737351f623fd13eb0b1f0e4c24
Andrew Cote <andrew.cote00@gmail.com>
2020-07-12 22:28:41 -0700

    fixed decrement bug, added csv-record making

---
+++ WellToWell.py
@@ -0,0 +1,376 @@
+#!/usr/bin/env python3
+# Joana Cabrera
+# 3/15/2020
+
+import logging, csv, time, os, re, json, uuid, datetime, argparse
+import pandas as pd
+import csv, re, uuid, datetime
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from WellLit.plateLighting import Well, PlateLighting
+from WellLit.Transfer import Transfer, TransferProtocol, TError, TStatus, TConfirm
+
+#TODO: regexp check on well names
+#TODO: Write log files
+
+DEST = 'destination-plate'
+
+
+class WelltoWell:
+	"""
+	Class for importing and validating a csv file to build a database of well-to-well transfers
+	* Loads a csv file into a pandas DataFrame, checking for duplicates or invalid Well labels
+	* Parses a validated DataFrame into a TransferProtocol
+	* Passes user commands (next, skip etc) to TransferProtocol
+	* Writes Transfers to transferlog.csv
+
+	Raises TError if user incorrectly specifies csv source file, or uses gui before transfer is loaded
+	"""
+
+	def __init__(self, csv=None):
+		self.csv = csv
+		self.msg = ''
+		self.df = None
+		self.tp = None
+		self.timestamp = ''
+
+		if self.csv is not None:
+			self.loadCsv(csv)
+
+	def tp_present(self):
+		if self.tp is not None:
+			return True
+		else:
+			self.log('No Transfer Protocol loaded. \n Load a CSV file to begin')
+			raise TError(self.msg)
+
+	def next(self):
+		if self.tp_present():
+			self.tp.complete()
+
+	def skip(self):
+		if self.tp_present():
+			self.tp.skip()
+
+	def failed(self):
+		if self.tp_present():
+			self.tp.failed()
+
+	def undo(self):
+		if self.tp_present():
+			self.tp.undo()
+
+	def nextPlate(self):
+		if self.tp_present():
+			self.tp.nextPlate()
+
+	def nextPlateOverride(self):
+		if self.tp_present():
+			self.tp.nextPlateOverride()
+
+	def nextPlateConfirm(self):
+		if self.tp_present():
+			self.tp.nextPlateConfirm()
+
+	def log(self, msg):
+		self.msg = msg
+		logging.info(msg)
+
+	def loadCsv(self, csv):
+		"""
+		Validates a csv file as being free of duplicates before loading constructing a TransferProtocol from it
+
+		:param csv: file to be uploaded
+
+		Raises TError if there are problems importing the file
+		Raises TConfirm if the file loads successfully
+		"""
+		try:
+			self.df = pd.read_csv(csv)
+			self.log('CSV file %s loaded' % csv)
+		except:
+			self.log('Failed to load file csv \n %s' % csv)
+			raise TError(self.msg)
+
+		hasSourDupes, msg_s = self.checkDuplicateSource()
+		hasDestDupes, msg_d = self.checkDuplicateDestination()
+
+		if hasSourDupes and hasDestDupes:
+			self.log('CSV file has duplicate wells in destinations and sources')
+			raise TError(self.msg)
+		if hasDestDupes:
+			self.log('CSV file has duplicate well destinations')
+			raise TError(self.msg)
+		if hasSourDupes:
+			self.log('CSV file has duplicate well sources')
+			raise TError(self.msg)
+		else:
+			self.tp = WTWTransferProtocol(df=self.df)
+			self.log('TransferProtocol with %s transfers \n in %s plates created' %
+					 (self.tp.num_transfers, self.tp.num_plates))
+			self.timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S')
+			raise TConfirm(self.msg)
+
+	def checkDuplicateDestination(self):
+		hasDupes = False
+		dupes_mask = self.df.duplicated(subset='TargetWell')
+		dupes = self.df.where(dupes_mask).dropna()
+		duplicates = set(dupes['TargetWell'].values)
+		error_msg = ''
+
+		if len(duplicates) == 0:
+			return hasDupes, error_msg
+		else:
+			hasDupes = True
+
+		for element in duplicates:
+			subset = self.df.where(self.df['TargetWell'] == element).dropna()
+
+			indices = []
+			for index, row in subset.iterrows():
+				indices.append(index)
+				target = row['TargetWell']
+			error_msg = error_msg + 'TargetWell %s is duplicated in rows %s ' % (target, indices)
+		return hasDupes, error_msg
+
+	def checkDuplicateSource(self):
+		plates = self.df['PlateName'].drop_duplicates().values
+
+		hasDupes = False
+		error_msg = []
+		msg = ''
+
+		for plate in plates:
+			batch = self.df.where(self.df['PlateName'] == plate).dropna()
+
+			dupes_mask = batch.duplicated(subset='SourceWell')
+			dupes = batch.where(dupes_mask).dropna()
+			duplicates = set(dupes['SourceWell'].values)
+
+			if len(duplicates) == 0:
+				continue
+			else:
+				hasDupes = True
+
+			message = 'Plate %s has duplicates: ' % plate
+			for element in duplicates:
+				subset = batch.where(batch['SourceWell'] == element).dropna()
+
+				indices = []
+				for index, row in subset.iterrows():
+					indices.append(index)
+				msg = msg + 'SourceWell %s is duplicated in rows %s' % (element, indices)
+
+			error_msg.append(msg)
+			msg = ''
+
+		return hasDupes, error_msg
+
+	def abortTransfer(self):
+		if self.tp_present():
+			self.tp = None
+			self.df = None
+
+	def writeTransferRecordFiles(self, _):
+		self.log('Generating records for ' + str(len(self.tp.transfers)) + ' transfers')
+		path = os.getcwd() + '/records/'
+		filename = 'transfer_record_' + self.timestamp + '.csv'
+		print(path + filename)
+		try:
+			with open(path + filename, mode='w') as logfile:
+				log_writer = csv.writer(logfile, delimiter=',')
+				log_writer.writerow(['Timestamp', 'Source plate', 'Source well', 'Destination plate', 'Destination well', 'Status'])
+				keys = ['timestamp', 'source_plate', 'source_well', 'dest_plate', 'dest_well', 'status']
+				for transfer_id in self.tp.tf_seq:
+					transfer = self.tp.transfers[transfer_id]
+					log_writer.writerow([transfer[key] for key in keys])
+			self.log('Wrote transfer record to ' + path + filename)
+		except:
+			raise TError('Cannot write log file to ' + path + filename)
+
+
+class WTWTransferProtocol(TransferProtocol):
+	"""
+	TransferProtocol that handles multiple source plates when transferring well-to-well.
+	Implements buildTransferProtocol, step from parent class.
+	Overrides default synchronize and canUpdate methods.
+
+	Raises TError if user tries to skip incomplete source plate
+	"""
+	def __init__(self, df=None, **kwargs):
+		super(WTWTransferProtocol, self).__init__(**kwargs)
+		self.transfers_by_plate = {}
+		self.df = df
+		self.msg = ''
+		if self.df is not None:
+			self.buildTransferProtocol(df)
+
+	def buildTransferProtocol(self, df):
+		if df is not None:
+			self.plate_names = df['PlateName'].unique()
+
+			# organize transfers into a dict by unique_id, collect id's into lists by plateName
+			for plate_name in self.plate_names:
+				plate_df = df[df['PlateName'] == plate_name]
+				plate_transfers = []
+				for idx, plate_df_entry in plate_df.iterrows():
+					src_plt = plate_df_entry[0]
+					dest_plt = DEST
+					src_well = plate_df_entry[1]
+					dest_well = plate_df_entry[2]
+					unique_id = str(uuid.uuid1())
+
+					tf = Transfer(unique_id,
+								  source_plate=src_plt, dest_plate=dest_plt, source_well=src_well, dest_well=dest_well,)
+					plate_transfers.append(unique_id)
+					self.transfers[unique_id] = tf
+
+				self.transfers_by_plate[plate_name] = plate_transfers
+
+			# produce numpy array of ids to be performed in a sequence, grouped by plate.
+			current_idx = 0
+			self.num_transfers = len(self.transfers)
+			self.tf_seq = np.empty(self.num_transfers, dtype=object)
+
+			for plate in self.plate_names:
+				plate = self.transfers_by_plate[plate]
+				for tf_id in plate:
+					self.tf_seq[current_idx] = tf_id
+					current_idx += 1
+
+			self._current_idx = 0  # index in tf_seq
+			self._current_plate = 0  # index in plate_names
+
+			self.current_uid = self.tf_seq[self._current_idx]
+			self.current_plate_name = self.plate_names[self._current_plate]
+			self.num_plates = len(self.plate_names)
+			self.plate_sizes = {}
+			for plate in self.plate_names:
+				self.plate_sizes[plate] = len(self.transfers_by_plate[plate])
+			self.sortTransfers()
+
+	def canUpdate(self):
+		"""
+		Checks to see that current transfer has not already been timestamped with a status.
+
+		Raises TError if transfer has already been updated and/or plate is complete
+		"""
+		self.synchronize()
+		self.sortTransfers()
+		current_transfer = self.transfers[self.current_uid]
+		if current_transfer['timestamp'] is None:
+			return True
+		else:
+			msg = ''
+			self.log('Cannot update transfer: %s \nStatus is already marked as %s \n' %
+					 (self.tf_id(), self.transfers[self.current_uid]['status']))
+			msg = self.msg
+			if self.plateComplete():
+				if self.protocolComplete():
+					self.log('Transfer Protocol complete \n')
+					raise TError(msg + self.msg)
+				else:
+					self.log('Plate %s is complete, press next plate to continue \n' % self.current_plate_name)
+					raise TError(msg + self.msg)
+
+	def step(self):
+		"""
+		Moves index to the next transfer in a plate. If plate full or transfer complete, raises flag
+		"""
+		self.sortTransfers()
+		self.canUndo = True
+
+		if self.plateComplete():
+			if self.protocolComplete():
+				self.log('TransferProtocol is complete')
+			else:
+				self.log('Plate %s completed' % self.current_plate_name)
+		else:
+			self.current_idx_increment()
+
+	def nextPlate(self):
+		"""
+		Checks if a source plate
+		Raise TConfirm if plate is complete
+		Raises TError if plate is incomplete
+		"""
+		if self.plateComplete():
+			raise TConfirm('Are you sure you wish to finish the plate?')
+		else:
+			self.log('Warning: Plate %s not yet complete \n' % self.current_plate_name)
+			skipped_transfers_in_plate = list(
+				set(self.lists['uncompleted']) &
+				set(self.transfers_by_plate[self.current_plate_name]))
+			msg = self.msg
+			self.log('Confirm to skip %s remaining transfers. \n Are you sure?' % len(
+				skipped_transfers_in_plate))
+			raise TError(msg + self.msg)
+
+	def nextPlateConfirm(self):
+		self.canUndo = False
+		if not self.protocolComplete():
+			self.current_plate_increment()
+			self.current_idx_increment()
+			self.log('Please load plate %s' % self.current_plate_name)
+			raise TConfirm(self.msg)
+		else:
+			self.log('TransferProtocol is complete')
+
+	def nextPlateOverride(self):
+		"""
+		Marks any incomplete transfers in the current plate as skipped and moves to the next plate
+
+		raises TConfirm to notify how many transfers skipped
+		"""
+		# collect leftover transfers
+		skipped_transfers_in_plate = list(
+			set(self.lists['uncompleted']) &
+			set(self.transfers_by_plate[self.current_plate_name]))
+
+		# Mark uncomplete transfers as skipped for this plate
+		for tf in skipped_transfers_in_plate:
+			self.transfers[tf].updateStatus(TStatus.skipped)
+
+		self.log('Remaining %s transfers in plate %s skipped' %
+				 (len(skipped_transfers_in_plate), self.current_plate_name))
+		if self.protocolComplete():
+			raise TConfirm(self.msg + ' \n Protocol Complete')
+		else:
+			self.current_plate_increment()
+			self.current_idx_increment(steps=len(skipped_transfers_in_plate))
+			raise TConfirm(self.msg)
+
+	def plateComplete(self):
+		self.synchronize()
+		self.sortTransfers()
+		for tf in self.transfers_by_plate[self.current_plate_name]:
+			if self.transfers[tf].status == TStatus.uncompleted:
+				return False
+		return True
+
+	def undo(self):
+		self.synchronize()
+		self.sortTransfers()
+		if self.canUndo:
+			if not self.plateComplete():
+				self.current_idx_decrement()
+			self.transfers[self.current_uid].resetTransfer()
+			self.sortTransfers()
+			self.canUndo = False
+			self.log('transfer marked incomplete: %s' % self.tf_id())
+		else:
+			self.log('Cannot undo previous operation')
+
+	def current_plate_increment(self):
+		self._current_plate += 1
+		self.synchronize()
+
+	def current_plate_decrement(self):
+		self._current_plate -= 1
+		self.synchronize()
+
+	def synchronize(self):
+		self.current_uid = self.tf_seq[self._current_idx]
+		self.current_plate_name = self.plate_names[self._current_plate]
+
--- WellToWellGUI.py
+++ WellToWellGUI.py
@@ -13,15 +13,12 @@ from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 from kivy.uix.filechooser import FileChooserListView
+from datetime import datetime
 import json, logging, time, os, time, csv
 from WellLit.WellLitGUI import WellLitWidget, ConfirmPopup, WellLitPopup, WellPlot
-from WelltoWell import WellToWell
+from WellLit.Transfer import TError, TConfirm
+from WellToWell import WelltoWell
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s [%(levelname)s] - %(message)s',
-    filename='filename.txt')  # pass explicit filename here
-logger = logging.getLogger()  # get the root logger
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
@@ -29,18 +26,30 @@ class LoadDialog(FloatLayout):
 
 
 class WelltoWellWidget(WellLitWidget):
+    """
+    Loads csv files as directed by user to build well-to-well transfer protocols, and provides functionality
+    for stepping through protocols using GUI buttons. Catches errors thrown by invalid user actions and displays error
+    message in popups.
+    """
     def __init__(self, **kwargs):
         super(WelltoWellWidget, self).__init__(**kwargs)
-        self.wtw = WellToWell()
+        self.wtw = WelltoWell()
+        self.initialized = False
 
     def load(self, path, filename):
         target = (os.path.join(str(path), str(filename[0])))
         logging.info('User selected file %s to load' % target)
         self.dismiss_popup()
         if os.path.isfile(target):
-            self.wtw.loadCsv(target)
-            if self.wtw.tp is not None:
-                self.reset_plates()
+            try:
+                self.wtw.loadCsv(target)
+            except TError as err:
+                self.showPopup(err, 'Load Failed')
+            except TConfirm as conf:
+                self.showPopup(conf, 'Load Successful')
+                if not self.initialized:
+                    self.reset_plates()
+                    self.initialized = True
                 self.updateLights()
                 self.wtw.tp.id_type = ''
 
@@ -49,68 +58,138 @@ class WelltoWellWidget(WellLitWidget):
         self._popup = Popup(title='Load File', content=content, size_hint=(0.5, 0.5))
         self._popup.open()
 
+    def reset_plates(self):
+        self.ids.source_plate.initialize()
+        self.ids.dest_plate.initialize()
+
     def updateLights(self):
         '''
-        dest_wells:
-            - colors well empty to filled, current target
-        source_wells:
-            - colors well filled to empty, current target
-        :return:
+        At each step:
+        dest_wells: completed -> filled, uncompleted -> empty
+        source_wells: completed -> empty, uncompletled -> full
+        color current target wells, and black out wells not involved in transfer
         '''
-        self.ids.source_plate.pl.emptyWells()
-        self.ids.dest_plate.pl.emptyWells()
-        current_transfers = self.wtw.tp.transfers_by_plate[self.wtw.tp.current_plate_name]
+        if self.initialized:
+            self.ids.source_plate.pl.emptyWells()
+            self.ids.dest_plate.pl.emptyWells()
+            current_transfers = self.wtw.tp.transfers_by_plate[self.wtw.tp.current_plate_name]
 
-        # For transfer that is complete, color source well empty and dest well as full
-        for tf_id in self.wtw.tp.lists['completed']:
-            self.ids.dest_plate.pl.markFilled(self.wtw.tp.transfers[tf_id]['dest_well'])
-            if tf_id in current_transfers:
-                self.ids.source_plate.pl.markEmpty(self.wtw.tp.transfers[tf_id]['source_well'])
+            # For transfer that is complete, color source well empty and dest well as full
+            for tf_id in self.wtw.tp.lists['completed']:
+                self.ids.dest_plate.pl.markFilled(self.wtw.tp.transfers[tf_id]['dest_well'])
+                if tf_id in current_transfers:
+                    self.ids.source_plate.pl.markEmpty(self.wtw.tp.transfers[tf_id]['source_well'])
 
-        # For transfer that is uncomplete, mark source well as full
-        for tf_id in list(set(current_transfers) & set(self.wtw.tp.lists['uncompleted'])):
-            self.ids.source_plate.pl.markFilled(self.wtw.tp.transfers[tf_id]['source_well'])
+            # For transfer that is uncomplete, mark source well as full
+            for tf_id in list(set(current_transfers) & set(self.wtw.tp.lists['uncompleted'])):
+                self.ids.source_plate.pl.markFilled(self.wtw.tp.transfers[tf_id]['source_well'])
 
-        # Mark current targets
-        self.ids.source_plate.pl.markTarget(self.wtw.tp.transfers[self.wtw.tp.current_uid]['source_well'])
-        self.ids.dest_plate.pl.markTarget(self.wtw.tp.transfers[self.wtw.tp.current_uid]['dest_well'])
+            # Mark current targets
+            self.ids.source_plate.pl.markTarget(self.wtw.tp.transfers[self.wtw.tp.current_uid]['source_well'])
+            self.ids.dest_plate.pl.markTarget(self.wtw.tp.transfers[self.wtw.tp.current_uid]['dest_well'])
 
-        self.ids.source_plate.pl.show()
-        self.ids.dest_plate.pl.show()
+            self.ids.source_plate.pl.show()
+            self.ids.dest_plate.pl.show()
+
+
+    def showPopup(self, error, title: str, func=None):
+        self._popup = WellLitPopup()
+        self._popup.size_hint = (0.3, .7)
+        self._popup.pos_hint = {'x': 10.0 / Window.width, 'y': 100 / Window.height}
+        self._popup.title = title
+        self._popup.show(error.__str__(), func=func)
 
     def next(self):
-        self.wtw.next()
-        self.updateLights()
+        try:
+            self.wtw.next()
+            self.updateLights()
+        except TError as err:
+            self.showPopup(err, 'Unable to complete transfer')
 
     def skip(self):
-        self.wtw.skip()
-        self.updateLights()
+        try:
+            self.wtw.skip()
+            self.updateLights()
+        except TError as err:
+            self.showPopup(err, 'Unable to skip transfer')
 
     def failed(self):
-        self.wtw.failed()
-        self.updateLights()
+        try:
+            self.wtw.failed()
+            self.updateLights()
+        except TError as err:
+            self.showPopup(err, 'Unable to abort transfer')
 
     def undo(self):
-        self.wtw.undo()
-        self.updateLights
+        try:
+            self.wtw.undo()
+            self.updateLights()
+        except TError as err:
+            self.showPopup(err, 'Unable to undo transfer')
 
     def nextPlate(self):
-        self.wtw.nextPlate()
-        self.updateLights()
+        try:
+            self.wtw.nextPlate()
+        except TError as err:
+            self.showPopup(err, 'Unable to complete plate', func=self.nextPlateOverride)
+            self.updateLights()
+        except TConfirm as conf:
+            self.showPopup(conf, 'Confirm finish plate', func=self.nextPlateConfirm)
+            self.updateLights()
+
+    def nextPlateOverride(self, _):
+        try:
+            self.wtw.nextPlateOverride()
+            self.updateLights()
+        except TError as err:
+            self.showPopup(err, 'Cannot override plate skip')
+            self.updateLights()
+        except TConfirm as conf:
+            self.showPopup(conf, 'Plate skipped')
+            self.updateLights()
+
+    def nextPlateConfirm(self, _):
+        try:
+            self.wtw.nextPlateConfirm()
+            self.updateLights()
+        except TError as err:
+            self.showPopup(err, 'Cannot load next plate')
+        except TConfirm as conf:
+            self.showPopup(conf, 'Load next plate')
+            self.updateLights()
+
+    def abortTransfer(self):
+        if self.initialized:
+            self.showPopup(TConfirm('Are you sure you wish to abort this transfer?'), 'Confirm transfer abort', func=self.abortTransferConfirm)
+
+    def abortTransferConfirm(self, _):
+        try:
+            self.ids.source_plate.pl.emptyWells()
+            self.ids.dest_plate.pl.emptyWells()
+            self.ids.source_plate.pl.show()
+            self.ids.dest_plate.pl.show()
+            self.showPopup(TConfirm('Would you like to generate a log file of the aborted protocol?'), 'Produce record', func=self.wtw.writeTransferRecordFiles)
+        except TError as err:
+            self.showPopup(err, 'Error aborting transfer')
 
 
-class WelltoWellApp(App):
+class WellToWellApp(App):
     def build(self):
         return WelltoWellWidget()
 
 
 if __name__ == '__main__':
     cwd = os.getcwd()
-    logging.basicConfig(filename=cwd + '\WelltoWell_log.log',
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-                        level=logging.INFO,
-                        datefmt='%Y-%m-%d %H:%M:%S')
+    logdir = os.getcwd() + '/logs/'
+    logfile = 'WelltoWell_Logfile_' + datetime.utcnow().strftime('%Y_%m_%d') + '.txt'
+    print(logdir + logfile)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] - %(message)s',
+        filename=logdir + logfile)  # pass explicit filename here
+    logger = logging.getLogger()  # get the root loggers
     logging.info('Session started')
     Window.size = (1600, 1200)
     # Window.fullscreen = True
-    WelltoWellApp().run()
+    WellToWellApp().run()
